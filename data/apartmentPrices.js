29a187518eee014f4cb727c4ee5d264223027a8c
mylomtl <153622682+mylomtl@users.noreply.github.com>
2024-10-13 17:41:06 +0200

    dgdgdfg
    dfggdfgdfgdg

---
+++ data/apartmentPrices.js
@@ -0,0 +1,30 @@
+const apartments = [
+	{
+		city: "Cape Town",
+		apartments: 1755.0,
+	},
+	{
+		city: "Istanbul",
+		apartments: 614.0,
+	},
+	{
+		city: "New York",
+		apartments: 5295.0,
+	},
+	{
+		city: "Paris",
+		apartments: 2079.0,
+	},
+	{
+		city: "Rio",
+		apartments: 3044.33,
+	},
+	{
+		city: "Sydney",
+		apartments: 860849.0,
+	},
+	{
+		city: "Tokyo",
+		apartments: 607.0,
+	},
+];
