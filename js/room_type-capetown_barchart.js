7a66e987945726cfd5d73b08349a1638ca798f89
philipp1902 <philipp.maginot@hfg-gmuend.de>
2024-10-15 19:59:57 +0200

    Refactor room type chart rendering in index.html and room_type-capetown_barchart.js

--- js/room_type-capetown_barchart.js
+++ js/room_type-capetown_barchart.js
@@ -107,3 +107,51 @@ d3.json("data/capetown2.json").then(function (data) {
 				.text("↑ Count of Room Types")
 		);
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	d3.json("data/capetown2.json").then(function (data) {
+		const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+		const width = 800 - margin.left - margin.right;
+		const height = 400 - margin.top - margin.bottom;
+
+		const svg = d3
+			.select("#room-type-chart")
+			.append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+			.attr("transform", `translate(${margin.left},${margin.top})`);
+
+		const x = d3
+			.scaleBand()
+			.domain(data.map(d => d.room_type))
+			.range([0, width])
+			.padding(0.1);
+
+		svg
+			.append("g")
+			.attr("transform", `translate(0,${height})`)
+			.call(d3.axisBottom(x));
+
+		const y = d3
+			.scaleLinear()
+			.domain([0, d3.max(data, d => d.count)])
+			.nice()
+			.range([height, 0]);
+
+		svg.append("g").call(d3.axisLeft(y));
+
+		svg
+			.selectAll(".bar")
+			.data(data)
+			.enter()
+			.append("rect")
+			.attr("class", "bar")
+			.attr("x", d => x(d.room_type))
+			.attr("y", d => y(d.count))
+			.attr("width", x.bandwidth())
+			.attr("height", d => height - y(d.count))
+			.attr("fill", "steelblue");
+	});
+});
